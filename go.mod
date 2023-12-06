c8a7edffdbb939424c11a3b6b874bb969ac77b02
mohemohe <mohemohe@ghippos.net>
2023-12-06 15:47:26 +0900

    add plugin feature

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/common-creation/lscbuild
 go 1.18
 
 require (
+	github.com/go-git/go-git/v5 v5.10.1
 	github.com/goccy/go-yaml v1.9.5
 	github.com/joho/godotenv v1.4.0
 	github.com/mitchellh/mapstructure v1.5.0
@@ -12,13 +13,33 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20230828082145-3c4c8a2d2371 // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.5.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pjbgf/sha1cd v0.3.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/skeema/knownhosts v1.2.1 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
-	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- lib/v1/executor.go
+++ lib/v1/executor.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/common-creation/lscbuild/typing"
 	"github.com/common-creation/lscbuild/util"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/goccy/go-yaml"
 	"github.com/mitchellh/mapstructure"
 	"github.com/samber/lo"
@@ -17,20 +19,33 @@ import (
 type (
 	Executor struct {
 		typing.Executor
-		lscBuild  *LSCBuild
-		globalEnv []string
+		lscBuild    *LSCBuild
+		globalEnv   []string
+		pluginCache map[string]*Plugin
 	}
 	LSCBuild struct {
 		typing.LSCBuild
-		Jobs yaml.MapSlice `yaml:"jobs"`
+		Jobs   yaml.MapSlice `yaml:"jobs"`
+		Plugin Plugin        `yaml:"plugin"`
 	}
 	Job struct {
 		Steps []Step   `yaml:"steps"`
 		Shell string   `yaml:"shell"`
 		Env   []string `yaml:"env"`
 	}
+	Plugin struct {
+		Job
+		LifeCycle PluginLifeCycle `yaml:"lifecycle"`
+		repoPath  string
+	}
+	PluginLifeCycle struct {
+		Initialize *Step `yaml:"initialize,omitempty"`
+		Run        *Step `yaml:"run,omitempty"`
+		Finalize   *Step `yaml:"finalize,omitempty"`
+	}
 	Step struct {
 		Name *string  `yaml:"name"`
+		Use  string   `yaml:"use"`
 		Cmd  string   `yaml:"cmd"`
 		Dir  string   `yaml:"dir"`
 		Env  []string `yaml:"env"`
@@ -45,6 +60,11 @@ type (
 		Exists  *string `yaml:"exists,omitempty"`
 		Missing *string `yaml:"missing,omitempty"`
 	}
+	GitRepoInfo struct {
+		GitRepo         string
+		ReferencePrefix string
+		ReferenceTarget string
+	}
 )
 
 func NewV1Executer(body []byte) (*Executor, error) {
@@ -53,8 +73,9 @@ func NewV1Executer(body []byte) (*Executor, error) {
 		return nil, err
 	}
 	return &Executor{
-		lscBuild:  lscBuild,
-		globalEnv: make([]string, 0),
+		lscBuild:    lscBuild,
+		globalEnv:   make([]string, 0),
+		pluginCache: make(map[string]*Plugin),
 	}, nil
 }
 
@@ -89,7 +110,18 @@ func (e *Executor) Run(jobs ...string) error {
 				return err
 			}
 
+			util.LogInfo("[lscbuild] initialize plugins\n")
+			if err := e.initializePlugins(job.Key.(string), decoded); err != nil {
+				return err
+			}
+
+			util.LogInfo("[lscbuild] execute jobs\n")
 			if err := e.runJob(job.Key.(string), decoded); err != nil {
+				return err
+			}
+
+			util.LogInfo("[lscbuild] finalize plugins\n")
+			if err := e.finalizePlugins(); err != nil {
 				return err
 			}
 		}
@@ -99,10 +131,20 @@ func (e *Executor) Run(jobs ...string) error {
 }
 
 func (e *Executor) stepName(step *Step) string {
+	var name string
 	if step.Name != nil && *step.Name != "" {
-		return *step.Name
+		name = *step.Name
+	} else if step.Use != "" {
+		name = step.Use
+	} else {
+		name = step.Cmd
 	}
-	return step.Cmd
+
+	name = strings.ReplaceAll(name, "\n", "; ")
+	if len(name) > 50 {
+		name = name[:50] + "..."
+	}
+	return name
 }
 
 func (e *Executor) prepareStep(name string, step *Step) bool {
@@ -165,6 +207,139 @@ func (e *Executor) prepareStep(name string, step *Step) bool {
 	return true
 }
 
+func (e *Executor) parsePluginUse(use string) GitRepoInfo {
+	var gitRepo string
+	var referencePrefix string
+	var referenceTarget string
+
+	if strings.Contains(use, "@") {
+		gitArgs := strings.Split(use, "@")
+		gitRepo = gitArgs[0]
+		referencePrefix = "refs/tags/"
+		referenceTarget = gitArgs[1]
+	} else if strings.Contains(use, "#") {
+		gitArgs := strings.Split(use, "#")
+		gitRepo = gitArgs[0]
+		referencePrefix = "refs/heads/"
+		referenceTarget = gitArgs[1]
+	} else {
+		gitRepo = use
+	}
+
+	return GitRepoInfo{
+		GitRepo:         gitRepo,
+		ReferencePrefix: referencePrefix,
+		ReferenceTarget: referenceTarget,
+	}
+}
+
+func (e *Executor) getRepoPath(gitRepo string) string {
+	repoPath := filepath.Join(os.TempDir(), "lscbuild-plugins")
+	lo.ForEach[string](strings.Split(gitRepo, "/"), func(s string, i int) {
+		repoPath = filepath.Join(repoPath, s)
+	})
+	return repoPath
+}
+
+func (e *Executor) initializePlugins(name string, job *Job) (result error) {
+	util.LogInfo("[lscbuild] check plugins: %s\n", name)
+	defer func() {
+		if err := recover(); err != nil {
+			switch e := err.(type) {
+			case string:
+				result = errors.New(e)
+			case error:
+				result = e
+			default:
+				result = errors.New("unknown plugin error")
+			}
+		}
+	}()
+
+	// NOTE: prepare plugins
+	tmpBaseDir := filepath.Join(os.TempDir(), "lscbuild-plugins")
+	os.MkdirAll(tmpBaseDir, os.ModePerm)
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, step := range job.Steps {
+		if step.Use != "" {
+			stepName := e.stepName(&step)
+			util.LogInfo("[lscbuild] fetch plugin: %s\n", stepName)
+
+			parsed := e.parsePluginUse(step.Use)
+			repoPath := e.getRepoPath(parsed.GitRepo)
+			if s, err := os.Stat(repoPath); err == nil && s.IsDir() {
+				panic("plugin '" + parsed.GitRepo + "' is already exists")
+			}
+
+			_, err := git.PlainClone(repoPath, false, &git.CloneOptions{
+				URL:           "https://github.com/" + parsed.GitRepo + ".git",
+				ReferenceName: plumbing.ReferenceName(parsed.ReferencePrefix + parsed.ReferenceTarget),
+				Progress:      os.Stdout,
+			})
+			if err != nil {
+				panic(err)
+			}
+
+			pluginYamlPath := filepath.Join(repoPath, ".lscbuild.yaml")
+			b, err := os.ReadFile(pluginYamlPath)
+			if err != nil {
+				result = err
+				return result
+			}
+			lscbuild := new(LSCBuild)
+			if err := yaml.Unmarshal(b, lscbuild); err != nil {
+				result = err
+				return result
+			}
+			e.pluginCache[parsed.GitRepo] = &lscbuild.Plugin
+			e.pluginCache[parsed.GitRepo].repoPath = repoPath
+
+			if e.pluginCache[parsed.GitRepo].LifeCycle.Initialize != nil {
+				initializeStep := e.pluginCache[parsed.GitRepo].LifeCycle.Initialize.MustCopy()
+				initializeStep.Dir = repoPath
+				initializeStep.Env = append(initializeStep.Env, "LSCBUILD_CWD="+cwd)
+
+				if err := e.runStep(&e.pluginCache[parsed.GitRepo].Job, &initializeStep); err != nil {
+					result = err
+					return result
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func (e *Executor) finalizePlugins() (result error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, v := range e.pluginCache {
+		if v.LifeCycle.Finalize != nil {
+			finalizeStep := v.LifeCycle.Finalize.MustCopy()
+			finalizeStep.Dir = v.repoPath
+			finalizeStep.Env = append(finalizeStep.Env, "LSCBUILD_CWD="+cwd)
+
+			defer func() {
+				os.RemoveAll(v.repoPath)
+			}()
+			if err := e.runStep(&v.Job, &finalizeStep); err != nil {
+				result = err
+				return result
+			}
+		}
+	}
+
+	return result
+}
+
 func (e *Executor) runJob(name string, job *Job) (result error) {
 	util.LogInfo("[lscbuild] start job: %s\n", name)
 	defer func() {
@@ -190,44 +365,82 @@ func (e *Executor) runJob(name string, job *Job) (result error) {
 		}
 	}
 
-	shellArgs := strings.Split(job.Shell, " ")
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 
 	for _, step := range job.Steps {
-		stepName := e.stepName(&step)
+		if step.Use != "" {
+			parsed := e.parsePluginUse(step.Use)
+			cache := e.pluginCache[parsed.GitRepo]
 
-		if isWindows {
-			if strings.Contains(job.Shell, "cmd") {
-				file, err := os.CreateTemp(os.TempDir(), "*.bat")
-				if err != nil {
-					panic(err)
+			if cache.LifeCycle.Run != nil {
+				newStep := cache.LifeCycle.Run.MustCopy()
+				for _, v := range step.Env {
+					newStep.Env = append(newStep.Env, v)
 				}
-				defer func() {
-					file.Close()
-					os.Remove(file.Name())
-				}()
+				newStep.Env = append(newStep.Env, "LSCBUILD_CWD="+cwd)
 
-				file.WriteString("@echo off\n" + step.Cmd)
-				file.Sync()
-				file.Close()
-				step.Cmd = file.Name()
-			}
-			if strings.Contains(job.Shell, "powershell") {
-				file, err := os.CreateTemp(os.TempDir(), "*.ps1")
-				if err != nil {
-					panic(err)
-				}
-				defer func() {
-					file.Close()
-					os.Remove(file.Name())
-				}()
-
-				file.WriteString(step.Cmd)
-				file.Sync()
-				file.Close()
-				step.Cmd = file.Name()
+				e.runStep(job, &newStep)
 			}
 		} else {
-			file, err := os.CreateTemp(os.TempDir(), "*.sh")
+			e.runStep(job, &step)
+		}
+	}
+
+	return result
+}
+
+func (e *Executor) runStep(job *Job, step *Step) (result error) {
+	defer func() {
+		if err := recover(); err != nil {
+			switch e := err.(type) {
+			case string:
+				result = errors.New(e)
+			case error:
+				result = e
+			default:
+				result = errors.New("unknown step error")
+			}
+		}
+	}()
+
+	isWindows := util.IsWindows()
+
+	if job == nil {
+		job = &Job{}
+	}
+	if job.Shell == "" {
+		if isWindows {
+			job.Shell = "cmd.exe /c"
+		} else {
+			job.Shell = "/bin/sh"
+		}
+	}
+
+	shellArgs := strings.Split(job.Shell, " ")
+
+	stepName := e.stepName(step)
+
+	if isWindows {
+		if strings.Contains(job.Shell, "cmd") {
+			file, err := os.CreateTemp(os.TempDir(), "*.bat")
+			if err != nil {
+				panic(err)
+			}
+			defer func() {
+				file.Close()
+				os.Remove(file.Name())
+			}()
+
+			file.WriteString("@echo off\n" + step.Cmd)
+			file.Sync()
+			file.Close()
+			step.Cmd = file.Name()
+		}
+		if strings.Contains(job.Shell, "powershell") {
+			file, err := os.CreateTemp(os.TempDir(), "*.ps1")
 			if err != nil {
 				panic(err)
 			}
@@ -241,51 +454,79 @@ func (e *Executor) runJob(name string, job *Job) (result error) {
 			file.Close()
 			step.Cmd = file.Name()
 		}
-
-		cmd := exec.Command(shellArgs[0], append(shellArgs[1:], step.Cmd)...)
-		if step.Dir != "" {
-			if absolutePath, err := filepath.Abs(step.Dir); err == nil {
-				cmd.Dir = absolutePath
-			}
+	} else {
+		file, err := os.CreateTemp(os.TempDir(), "*.sh")
+		if err != nil {
+			panic(err)
 		}
+		defer func() {
+			file.Close()
+			os.Remove(file.Name())
+		}()
 
-		env := os.Environ()
-		env = append(env, "LSCBUILD=1")
-
-		if len(job.Env) > 0 {
-			env = append(env, job.Env...)
-		}
-		if len(step.Env) > 0 {
-			env = append(env, step.Env...)
-		}
-		env = append(env, e.globalEnv...)
-		env = append(env, "SHELL="+job.Shell)
-
-		cmd.Env = env
-		step.Env = env
-
-		if !e.prepareStep(stepName, &step) {
-			util.LogInfo("[lscbuild] skip step: %s\n", stepName)
-			continue
-		}
-
-		cmd.Stdin = nil
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		util.LogInfo("[lscbuild] start step: %s\n", stepName)
-
-		if err := cmd.Start(); err != nil {
-			result = err
-			return result
-		}
-		if err := cmd.Wait(); err != nil {
-			result = err
-			return result
-		}
-
-		util.LogInfo("[lscbuild] end step: %s\n", stepName)
+		file.WriteString(step.Cmd)
+		file.Sync()
+		file.Close()
+		step.Cmd = file.Name()
 	}
 
+	cmd := exec.Command(shellArgs[0], append(shellArgs[1:], step.Cmd)...)
+	if step.Dir != "" {
+		if absolutePath, err := filepath.Abs(step.Dir); err == nil {
+			cmd.Dir = absolutePath
+		}
+	}
+
+	env := os.Environ()
+	env = append(env, "LSCBUILD=1")
+
+	if len(job.Env) > 0 {
+		env = append(env, job.Env...)
+	}
+	if len(step.Env) > 0 {
+		env = append(env, step.Env...)
+	}
+	env = append(env, e.globalEnv...)
+	env = append(env, "SHELL="+job.Shell)
+
+	cmd.Env = env
+	step.Env = env
+
+	if !e.prepareStep(stepName, step) {
+		util.LogInfo("[lscbuild] skip step: %s\n", stepName)
+		return result
+	}
+
+	cmd.Stdin = nil
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	util.LogInfo("[lscbuild] start step: %s\n", stepName)
+
+	if err := cmd.Start(); err != nil {
+		result = err
+		return result
+	}
+	if err := cmd.Wait(); err != nil {
+		result = err
+		return result
+	}
+
+	util.LogInfo("[lscbuild] end step: %s\n", stepName)
+
 	return result
+}
+
+func (s *Step) MustCopy() Step {
+	// FIXME: slow zone
+
+	b, err := yaml.Marshal(*s)
+	if err != nil {
+		panic(err)
+	}
+	step := new(Step)
+	if err := yaml.Unmarshal(b, step); err != nil {
+		panic(err)
+	}
+	return *step
 }
